e7e81c657b6e575c722a1fce53ec430ccf09de80
ammilten <ammilten@stanford.edu>
2020-07-06 15:34:02 -0700

    added ability to only simulate incomplete or imported simulations

--- ERTFaultMonteCarlo.py
+++ ERTFaultMonteCarlo.py
@@ -1,93 +1,5 @@
-import scipy.stats as st
-import matplotlib.pyplot as plt
-import numpy as np
-from ERTsim import PHert
-from multiprocessing import Pool
-import time
+import MonteCarlo
 import sys, getopt, os
-
-# ---------------------- Construct Distributions ---------------------
-def construct_dists(dip, H, xpos, rho_fault, rho_back):
-    dipd = st.uniform(loc=dip[0], scale=dip[1]-dip[0])
-    Hd = st.norm(loc=H[0], scale=H[1])
-    xposd = st.norm(loc=xpos[0], scale=xpos[1])
-    rho_faultd = st.lognorm(rho_fault[2],loc=rho_fault[0], scale=rho_fault[1])
-    rho_backd = st.lognorm(rho_back[2],loc=rho_back[0], scale=rho_back[1])
-
-    return dipd, Hd, xposd, rho_faultd, rho_backd
-
-# ------------- Sample & Save Parameters ---------------------
-def save_params(dep, xtra, Q, dip, H, xpos, rho_fault, rho_back, N, MCfolder, override):
-    dipd, Hd, xposd, rho_faultd, rho_backd = construct_dists(dip, H, xpos, rho_fault, rho_back)
-    
-    dips = dipd.rvs(N)
-    Hs = Hd.rvs(N)
-    xposs = xposd.rvs(N)
-    rho_faults = rho_faultd.rvs(N)
-    rho_backs = rho_backd.rvs(N)
-
-    try:
-        os.mkdir(MCfolder)
-    except FileExistsError:
-        if override:
-            print("MC folder already exists. Overriding.")
-        else:
-            print("MC folder already exists. Aborting. Pass the argument -o to override")
-            sys.exit(2)
-
-    with open(MCfolder+'params.dat',"w") as pf:
-        pf.write(str(dep)+"\t#dep\n")
-        pf.write(str(xtra)+"\t#xtra\n")
-        pf.write(str(Q)+"\t#Q\n")
-        pf.write(str(dip[0])+", "+str(dip[1])+"\t#dip (uniform): lower, upper\n")
-        pf.write(str(H[0])+", "+str(H[1])+"\t#H (normal): mean, std\n")
-        pf.write(str(xpos[0])+", "+str(xpos[1])+"\t#xpos (normal): mean, std\n")
-        pf.write(str(rho_fault[0])+", "+str(rho_fault[1])+", "+str(rho_fault[2])+"\t#rho_fault (lognormal): loc, scale, s (see scipy.stats.lognorm docs)\n")
-        pf.write(str(rho_back[0])+", "+str(rho_back[1])+", "+str(rho_back[2])+"\t#rho_back (lognormal): loc, scale, s (see scipy.stats.lognorm docs)\n\n")
-        pf.write(str(N)+"\t#N\n")
-        pf.write("dip, H, xpos, rho_fault, rho_back\n")
-        for i in range(N):
-            pf.write(str(dips[i])+", "+str(Hs[i])+", "+str(xposs[i])+", "+str(rho_faults[i])+", "+str(rho_backs[i])+"\n")
-
-    PARAMS = list(zip(list(range(N)), dips, Hs, xposs, rho_faults, rho_backs, [dep]*N, [xtra]*N, [Q]*N))
-    return PARAMS
-
-# ------------- Wrapper to run the simulation with one argument ---------------
-def run(params):
-    print("Simulating realization " + str(params[0]))
-    real = real = MCfolder + "data_" + str(params[0]) + ".dat"
-    PH = PHert(dip=params[1], H=params[2], xpos=params[3], rho_fault=params[4], rho_back=params[5], dep=params[6], xtra=params[7], Q=params[8], outfile=real)
-    PH.run_full()
-    return
-
-# ------------- Plots the 5 distributions --------------------
-def plot_dists(dip, H, xpos, rho_fault, rho_back):
-    dipd, Hd, xposd, rho_faultd, rho_backd = construct_dists(dip, H, xpos, rho_fault, rho_back)
-
-    fig, axs = plt.subplots(5,1)
-
-    x = np.linspace(dipd.ppf(0.001)-10, dipd.ppf(0.999)+10, 1000)
-    axs[0].plot(x,dipd.pdf(x))
-    axs[0].set_xlabel('Dip')
-
-    x = np.linspace(Hd.ppf(0.001), Hd.ppf(0.999), 1000)
-    axs[1].plot(x,Hd.pdf(x))
-    axs[1].set_xlabel('H')
-    
-    x = np.linspace(xposd.ppf(0.001), xposd.ppf(0.999), 1000)
-    axs[2].plot(x,xposd.pdf(x))
-    axs[2].set_xlabel('xpos')
-    
-    x = np.linspace(rho_faultd.ppf(0.001), rho_faultd.ppf(0.999), 1000)
-    axs[3].plot(x,rho_faultd.pdf(x))
-    axs[3].set_xlabel('rho_fault')
-    
-    x = np.linspace(rho_backd.ppf(0.001), rho_backd.ppf(0.999), 1000)
-    axs[4].plot(x,rho_backd.pdf(x))
-    axs[4].set_xlabel('rho_back')
-    
-    fig.tight_layout()
-    plt.show()
 
 # ---------------- Parse arguments ---------------------
 def get_args(argv):
@@ -98,16 +10,17 @@ def get_args(argv):
     nproc = None
     MCfolder = ''
     foundMC = False
-    override = False
+    overwrite = False
+    imp = False
 
     try:
-        opts, args = getopt.getopt(argv,"hf:q:n:p:do",["mcfolder=","quality=","nreals=","showdists","parallel","override"])
+        opts, args = getopt.getopt(argv,"hf:q:n:pdoi",["mcfolder=","quality=","nreals=","showdists","parallel","overwrite","import"])
     except getopt.GetoptError:
         print("ERTMonteCarlo.py -f <path_to_mcfolder> -q <min_angle> -n <num_reals> -p <num_processors> -d -o")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("\nUSAGE:\npython ERTFaultMonteCarlo.py -f <path_to_mcfolder> -n <num_reals> -q <quality>\n\nSWITCHES:\n  -f, --mcfolder <folder>: Path to Monte Carlo output folder (required)\n  -q, --quality <min_angle>: Minimum mesh angle. 14=low quality, 34=high quality (default=14)\n  -n, --nreals <num_reals>: Number of realizations (default=1)\n  -p, --parallel <num_processors>: If specified, run in parallel. Option to specify number of of processors. If number of processors is not specified, it is auto-selected.\n  -d, --showdists: If specified, plot the parameter distributions prior to simulations.\n  -o, --override: If specified, the Monte Carlo will overwrite the data in MCfolder if MCfolder already exists.")
+            print("\nBASIC USAGE:\npython ERTFaultMonteCarlo.py -f <path_to_mcfolder> -n <num_reals>\n\nSWITCHES:\n  -f, --mcfolder <folder>: Path to Monte Carlo output folder (required)\n  -q, --quality <min_angle>: Minimum mesh angle. 14=low quality, 34=high quality (default=14)\n  -n, --nreals <num_reals>: Number of realizations (default=1)\n  -p, --parallel <num_processors>: If specified, run in parallel. Option to specify number of of processors. If number of processors is not specified, it is auto-selected.\n  -d, --showdists: If specified, plot the parameter distributions prior to simulations.\n  -o, --override: If specified, the Monte Carlo will overwrite the data in MCfolder if MCfolder already exists.\n  -i, --import: If specified, the program will attempt to load Monte Carlo parameters from <MCfolder>/params.dat.")
             sys.exit(2)
         elif opt in ("-q", "--quality"):
             Q = float(arg)
@@ -118,8 +31,10 @@ def get_args(argv):
             foundMC = True
         elif opt in ("-d", "--showdists"):
             showDists = True
-        elif opt in ("-o", "--override"):
-            override = True
+        elif opt in ("-o", "--overwrite"):
+            overwrite = True
+        elif opt in ("-i", "--import"):
+            imp = True
         elif opt in ("-p", "--parallel"):
             parallel = True
             try:
@@ -130,6 +45,9 @@ def get_args(argv):
     if not foundMC:
         print("MCfolder argument required: use '-f <path_to_mcfolder>' or '--mcfolder <path_to_mcfolder>'")
         sys.exit(2)
+
+    if not MCfolder.endswith('/'):
+        MCfolder = MCfolder+'/'
 
     print("MCfolder = "+MCfolder)
     print("Minimum angle = " + str(Q) + "    (14 = low quality mesh, 34 = high quality mesh)")
@@ -145,9 +63,11 @@ def get_args(argv):
         print("Plotting distributions = true")
     else:
         print("Plotting distributions = false")
+    if imp:
+        print("\n** Simulation will be imported from '" + MCfolder + "params.dat'.\n   Mesh quality and number of realizations above may be incorrect.")
 
     print("")
-    return MCfolder, Q, N, showDists, parallel, nproc, override
+    return MCfolder, Q, N, showDists, parallel, nproc, overwrite, imp
 
 
 
@@ -155,7 +75,7 @@ def get_args(argv):
 if __name__ == "__main__":
 
     print("\n----------- Preparing Monte Carlo -----------")
-    MCfolder, Q, N, showDists, parallel, nproc, override = get_args(sys.argv[1:])
+    MCfolder, Q, N, showDists, parallel, nproc, overwrite, imp = get_args(sys.argv[1:])
 
 #    MCfolder = '/home/ammilten/Programs/ERTFaultMonteCarlo/data/MC/'
 
@@ -168,31 +88,10 @@ if __name__ == "__main__":
     dep = 200
     xtra = 1000
 
-    np.random.seed()
-
-
-    # Sample & save
-    PARAMS = save_params(dep, xtra, Q, dip, H, xpos, rho_fault, rho_back, N, MCfolder, override)
-
-    # Plot distributions (optional)
-    if showDists:
-        plot_dists(dip, H, xpos, rho_fault, rho_back)
-    
-    # Simulate
-    print("-------------- Simulating --------------")
-    tstart = time.time()
-    if parallel:
-        if nproc is None:
-            pool = Pool()
-            pool.map(run, PARAMS)
-        else:
-            pool = Pool(processes=nproc)
-            pool.map(run, PARAMS)
+    if imp:
+        MC = MonteCarlo.import_simulation(MCfolder,overwrite, parallel, nproc, showDists, hasSeed=True)
     else:
-        for i in range(N): 
-            #print("--------- Working on " + str(i+1) + " of " + str(N)+" ---------") 
-            run(PARAMS[i])
-    tend = time.time()
-    print("\nTime to simulate " + str(N) + " realizations: " + str(np.round((tend-tstart)/60,2)) + " minutes")
+        MC = MonteCarlo.MonteCarlo(dep, xtra, Q, dip, H, xpos, rho_fault, rho_back, N, MCfolder, overwrite, parallel, nproc, showDists)
 
+    MC.run()
 
---
+++ MonteCarlo.py
@@ -0,0 +1,237 @@
+import scipy.stats as st
+import matplotlib.pyplot as plt
+import numpy as np
+from ERTsim import PHert
+from multiprocessing import Pool
+import os, sys, time
+from os import path
+
+
+# ------------- Wrapper to run a realization ---------------
+def realize(params):
+    print("Simulating realization " + str(params[0]))
+    MCfolder = params[9]
+    real = MCfolder + "data_" + str(params[0]) + ".dat"
+
+    PH = PHert(dip=params[1], H=params[2], xpos=params[3], rho_fault=params[4], rho_back=params[5], dep=params[6], xtra=params[7], Q=params[8], outfile=real)
+    PH.run_full()
+    return
+
+# ------------- Parse Parameters ---------------------
+def import_simulation(MCfolder, overwrite=False, parallel=False, nproc=None, showDists=False, hasSeed=True): 
+    if MCfolder.endswith('/'):
+        pfile = MCfolder + 'params.dat'
+    else:
+        pfile = MCfolder + '/params.dat'
+    if not path.exists(pfile):
+        print("Error: " + pfile + " does not exist. Aborting")
+        sys.exit(2)
+
+    with open(pfile,"r") as pf: # Might need to re-do to account for whitespace
+        comm = '#'
+        sep = ','
+
+        dep = float(pf.readline().split(comm,1)[0])
+        xtra = float(pf.readline().split(comm,1)[0])
+        Q = float(pf.readline().split(comm,1)[0])
+
+        dipln = pf.readline().split(comm,1)[0].split(sep)
+        dip = [float(dipln[0]), float(dipln[1])]
+
+        Hln = pf.readline().split(comm,1)[0].split(sep)
+        H = [float(Hln[0]), float(Hln[1])]
+
+        xposln = pf.readline().split(comm,1)[0].split(sep)
+        xpos = [float(xposln[0]), float(xposln[1])]
+
+        rho_faultln = pf.readline().split(comm,1)[0].split(sep)
+        rho_fault = [float(rho_faultln[0]), float(rho_faultln[1]), float(rho_faultln[2])]
+
+        rho_backln = pf.readline().split(comm,1)[0].split(sep)
+        rho_back = [float(rho_backln[0]), float(rho_backln[1]), float(rho_backln[2])]
+
+        pf.readline() # THIS LINE ASSUMES THERE IS A SPACE
+        if hasSeed:
+            seed = int(pf.readline().split(comm,1)[0])
+        else:
+            seed = 0
+        N = int(pf.readline().split(comm,1)[0])
+
+        pf.readline() # Skip the header
+        dips = [None]*N
+        Hs = [None]*N
+        xposs = [None]*N
+        rho_faults = [None]*N
+        rho_backs = [None]*N
+        for i in range(N):
+            params = pf.readline().split(sep)
+            dips[i] = float(params[0])
+            Hs[i] = float(params[1])
+            xposs[i] = float(params[2])
+            rho_faults[i] = float(params[3])
+            rho_backs[i] = float(params[4])
+
+        PARAMS = list(zip(list(range(N)), dips, Hs, xposs, rho_faults, rho_backs, [dep]*N, [xtra]*N, [Q]*N, [MCfolder]*N))
+
+    MC = MonteCarlo(dep, xtra, Q, dip, H, xpos, rho_fault, rho_back, N, MCfolder, overwrite=overwrite, parallel=parallel, nproc=nproc, showDists=showDists, seed=seed, saveParams=False) 
+    MC.PARAMS = PARAMS
+
+    return MC
+
+class MonteCarlo:
+    def __init__(self, dep, xtra, Q, dip, H, xpos, rho_fault, rho_back, N, MCfolder, overwrite=False, parallel=False, nproc=None, showDists=False, seed=0, saveParams=True):
+        self.dep = dep
+        self.xtra = xtra
+        self.Q = Q
+        self.dip = dip
+        self.H = H
+        self.xpos = xpos
+        self.rho_fault = rho_fault
+        self.rho_back = rho_back
+        self.N = N
+        self.overwrite = overwrite
+        self.parallel = parallel
+        self.nproc = nproc
+        self.showDists = showDists  
+        if MCfolder.endswith('/'):
+            self.MCfolder = MCfolder
+        else:
+            self.MCfolder = MCfolder + '/'
+        self.parameterfile = self.MCfolder + 'params.dat'
+        self.seed = seed
+
+        np.random.seed(self.seed)
+        self.construct_dists()
+        if saveParams:
+            self.save_params()
+        return
+
+    # ---------------------- Construct Distributions ---------------------
+    def construct_dists(self):
+        self.dipd = st.uniform(loc=self.dip[0], scale=self.dip[1]-self.dip[0])
+        self.Hd = st.norm(loc=self.H[0], scale=self.H[1])
+        self.xposd = st.norm(loc=self.xpos[0], scale=self.xpos[1])
+        self.rho_faultd = st.lognorm(self.rho_fault[2],loc=self.rho_fault[0], scale=self.rho_fault[1])
+        self.rho_backd = st.lognorm(self.rho_back[2],loc=self.rho_back[0], scale=self.rho_back[1])
+        return
+
+    # ------------- Sample & Save Parameters ---------------------
+    def save_params(self):     
+    
+        dips = self.dipd.rvs(self.N)
+        Hs = self.Hd.rvs(self.N)
+        xposs = self.xposd.rvs(self.N)
+        rho_faults = self.rho_faultd.rvs(self.N)
+        rho_backs = self.rho_backd.rvs(self.N)
+
+        try:
+            os.mkdir(self.MCfolder)
+        except FileExistsError:
+            if self.overwrite:
+                print("MC folder already exists. Overwriting.")
+            else:
+                print("MC folder already exists. Only simulating incomplete realizations. Pass the switch '-o' to overwrite all realizations.")
+    
+        with open(self.parameterfile,"w") as pf:
+            pf.write(str(self.dep)+"\t#dep\n")
+            pf.write(str(self.xtra)+"\t#xtra\n")
+            pf.write(str(self.Q)+"\t#Q\n")
+            pf.write(str(self.dip[0])+", "+str(self.dip[1])+"\t#dip (uniform): lower, upper\n")
+            pf.write(str(self.H[0])+", "+str(self.H[1])+"\t#H (normal): mean, std\n")
+            pf.write(str(self.xpos[0])+", "+str(self.xpos[1])+"\t#xpos (normal): mean, std\n")
+            pf.write(str(self.rho_fault[0])+", "+str(self.rho_fault[1])+", "+str(self.rho_fault[2])+"\t#rho_fault (lognormal): loc, scale, s (see scipy.stats.lognorm docs)\n")
+            pf.write(str(self.rho_back[0])+", "+str(self.rho_back[1])+", "+str(self.rho_back[2])+"\t#rho_back (lognormal): loc, scale, s (see scipy.stats.lognorm docs)\n\n")
+            pf.write(str(self.seed)+"\t#random number seed\n")
+            pf.write(str(self.N)+"\t#N\n")
+            pf.write("dip, H, xpos, rho_fault, rho_back\n")
+            for i in range(self.N):
+                pf.write(str(dips[i])+", "+str(Hs[i])+", "+str(xposs[i])+", "+str(rho_faults[i])+", "+str(rho_backs[i])+"\n")
+
+        self.PARAMS = list(zip(list(range(self.N)), dips, Hs, xposs, rho_faults, rho_backs, [self.dep]*self.N, [self.xtra]*self.N, [self.Q]*self.N, [self.MCfolder]*self.N))
+        return self.PARAMS
+
+
+    # ------------- Plots the 5 distributions --------------------
+    def plot_dists(self):
+        fig, axs = plt.subplots(5,1)
+
+        x = np.linspace(self.dipd.ppf(0.001)-10, self.dipd.ppf(0.999)+10, 1000)
+        axs[0].plot(x,self.dipd.pdf(x))
+        axs[0].set_xlabel('Dip')
+    
+        x = np.linspace(self.Hd.ppf(0.001), self.Hd.ppf(0.999), 1000)
+        axs[1].plot(x,self.Hd.pdf(x))
+        axs[1].set_xlabel('H')
+    
+        x = np.linspace(self.xposd.ppf(0.001), self.xposd.ppf(0.999), 1000)
+        axs[2].plot(x,self.xposd.pdf(x))
+        axs[2].set_xlabel('xpos')
+    
+        x = np.linspace(self.rho_faultd.ppf(0.001), self.rho_faultd.ppf(0.999), 1000)
+        axs[3].plot(x,self.rho_faultd.pdf(x))
+        axs[3].set_xlabel('rho_fault')
+    
+        x = np.linspace(self.rho_backd.ppf(0.001), self.rho_backd.ppf(0.999), 1000)
+        axs[4].plot(x,self.rho_backd.pdf(x))
+        axs[4].set_xlabel('rho_back')
+    
+        fig.tight_layout()
+        plt.show()
+        return
+
+    # ------------- Wrapper to run a realization ---------------
+    def realize(self, i):
+        print("Simulating realization " + str(self.PARAMS[i][0]))
+        real = self.MCfolder + "data_" + str(self.PARAMS[i][0]) + ".dat"
+        PH = PHert(dip=self.PARAMS[i][1], H=self.PARAMS[i][2], xpos=self.PARAMS[i][3], rho_fault=self.PARAMS[i][4], rho_back=self.PARAMS[i][5], dep=self.PARAMS[i][6], xtra=self.PARAMS[i][7], Q=self.PARAMS[i][8], outfile=real)
+        PH.run_full()
+        return
+
+    # ------------- Find realizations that have already been run -------
+    def find_incomplete_reals(self):
+        # find existing data files
+        incomplete = []
+        for i in range(self.N):
+            real = self.MCfolder + 'data_' + str(i) + '.dat'
+            if not path.exists(real):
+                incomplete.append(i)
+        return incomplete
+
+
+    # ------------------ Runs full Monte Carlo ----------------
+    def run(self):
+        incomplete = self.find_incomplete_reals()
+        if self.overwrite:
+            print("All "+str(self.N) + " Realizations will be overwritten.")
+            PARAMS = self.PARAMS
+        else:
+            if len(incomplete) > 0:
+                print(str(len(incomplete)) + " incomplete realizations will be simulated.")
+                PARAMS = [self.PARAMS[i] for i in incomplete]
+            else:
+                print("0 incomplete realizations. Exiting.")
+                sys.exit()
+
+    
+        # Plot distributions (optional)
+        if self.showDists:
+            self.plot_dists()
+    
+        # Simulate
+        print("-------------- Simulating " + str(len(PARAMS)) + " Realizations --------------")
+        tstart = time.time()
+        if self.parallel:
+            if self.nproc is None:
+                pool = Pool()
+                pool.map(realize, PARAMS)
+            else:
+                pool = Pool(processes=self.nproc)
+                pool.map(realize, PARAMS)
+        else:
+            for i in range(len(PARAMS)): 
+                #print("--------- Working on " + str(i+1) + " of " + str(N)+" ---------") 
+                realize(PARAMS[i])
+        tend = time.time()
+        print("\nTime to simulate " + str(len(PARAMS)) + " realizations: " + str(np.round((tend-tstart)/60,2)) + " minutes")
+        return
+
