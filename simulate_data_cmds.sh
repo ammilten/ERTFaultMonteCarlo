cd043fb3a36076427b9b71f37062d4ee53e7cee4
ammilten <ammilten@stanford.edu>
2021-04-04 20:30:39 -0700

    commands to simulate ERT data in paper

---
+++ simulate_data_cmds.sh
@@ -0,0 +1,7 @@
+python ERTFaultMonteCarlo.py -f data/MC3 -i # Prior data set, serial implementation
+#python ERTFaultMonteCarlo.py -f data/MC3 -p 8 -i # Prior data set, with 8 processors
+
+python ERTFaultMonteCarlo.py -f data/val -i # Validation/posterior falsification data set, serial implementation
+#python ERTFaultMonteCarlo.py -f data/val -p 8 -i # Validation/posterior falsification data set, with 8 processors
+
+
